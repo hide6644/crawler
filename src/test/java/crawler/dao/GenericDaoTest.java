05966c6d1e2a4fdfb79caba5d3274cfd20e3aeae
hide6644 <hide6644@hotmail.com>
2017-06-10 23:30:55 +0900

    Unit Testの実装 #1
    テストメソッドを修正した。

--- src/test/java/crawler/dao/GenericDaoTest.java
+++ src/test/java/crawler/dao/GenericDaoTest.java
@@ -2,9 +2,12 @@ package crawler.dao;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceContext;
 
 import org.junit.Before;
@@ -25,24 +28,42 @@ public class GenericDaoTest extends BaseDaoTestCase {
     @Before
     public void setUp() {
         genericDao = new GenericDaoJpa<Novel, Long>(Novel.class, entityManager);
+
+        Novel novel = new Novel();
+        novel.setUrl("Url");
+        novel.setTitle("Title");
+        novel.setWritername("Writername");
+        novel.setDescription("Description");
+        novel.setBody("Body");
+        genericDao.save(novel);
     }
 
     @Test
     public void testGet() {
-        Novel novel = genericDao.get(1L);
+        Map<String, Object> param = new HashMap<>();
+        param.put("url", "Url");
+        List<Novel> novelList = genericDao.findByNamedQuery("Novel.findByUrl", param);
+        assertNotNull(novelList);
+        assertTrue(genericDao.exists(novelList.get(0).getId()));
+
+        Novel novel = genericDao.get(novelList.get(0).getId());
         assertNotNull(novel);
-        assertTrue(genericDao.exists(novel.getId()));
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void testGetInvalid() throws Exception {
+        genericDao.get(-1L);
     }
 
     @Test
     public void testGetAll() {
         List<Novel> novelList = genericDao.getAll();
-        assertTrue(novelList.size() > 1);
+        assertTrue(novelList.size() > 0);
     }
 
     @Test
     public void testGetAllDistinct() {
         List<Novel> novelDistinctList = genericDao.getAllDistinct();
-        assertTrue(novelDistinctList.size() > 1);
+        assertTrue(novelDistinctList.size() > 0);
     }
 }
