4557f857e6dfec2a36e9ea85b1a78dfe80d30e59
hide6644 <hide6644@gmail.com>
2017-10-19 13:27:58 +0900

    Travis CIの設定
    File.separatorに変更した。

--- src/main/java/crawler/Constants.java
+++ src/main/java/crawler/Constants.java
@@ -1,5 +1,6 @@
 package crawler;
 
+import java.io.File;
 import java.util.ResourceBundle;
 
 /**
@@ -18,7 +19,7 @@ public class Constants {
     public static final String BUNDLE_KEY = "applicationResources";
 
     /** ファイル区切り文字 */
-    public static final String FILE_SEP = System.getProperty("file.separator");
+    public static final String FILE_SEP = File.separator;
 
     /** 実行フォルダ名 */
     public static final String APP_FOLDER_NAME = System.getProperty("app_home") + Constants.FILE_SEP;
