a5ec1aa6ae8acfff55fa68c59ab08200cbda5504
hide6644 <hide6644@gmail.com>
2017-12-15 12:56:53 +0900

    Unit Testの実装 #1
    テストケースを追加した。

---
+++ src/test/java/crawler/service/impl/NovelChapterInfoManagerImplTest.java
@@ -0,0 +1,23 @@
+package crawler.service.impl;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+import crawler.domain.NovelChapter;
+import crawler.domain.source.NovelChapterSource;
+import crawler.domain.source.NovelIndexElement;
+
+public class NovelChapterInfoManagerImplTest {
+
+    @Test
+    public void testSaveNovelInfo() throws Exception {
+        NovelIndexElement novelIndexElement = mock(NovelIndexElement.class);
+        NovelChapterSource novelChapterSource = mock(NovelChapterSource.class);
+        when(novelIndexElement.getChapterModifiedDate()).thenReturn("2016/09/23 00:00");
+        when(novelChapterSource.getNovelChapter()).thenReturn(new NovelChapter());
+
+        NovelChapterInfoManagerImpl novelChapterInfoManager = new NovelChapterInfoManagerImpl();
+        novelChapterInfoManager.saveNovelChapterInfo(novelIndexElement, novelChapterSource);
+    }
+}
