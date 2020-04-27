3647cc578a2d7d00e9ea65ad7c46c6cd841c3f24
hide6644 <hide6644@gmail.com>
2020-04-28 00:45:24 +0900

    リファクタリング
    テストケースを修正した。

--- src/test/java/crawler/entity/NovelTest.java
+++ src/test/java/crawler/entity/NovelTest.java
@@ -28,6 +28,38 @@ public class NovelTest {
     }
 
     @Test
+    public void testNovelChapters() {
+        Novel novel = new Novel();
+        NovelChapter novelChapter1 = new NovelChapter();
+        novelChapter1.setUrl("test1");
+        novel.addNovelChapter(novelChapter1);
+
+        assertEquals(1, novel.getNovelChapters().size());
+
+        NovelChapter novelChapter2 = new NovelChapter();
+        novelChapter2.setUrl("test1");
+        novel.removeNovelChapter(novelChapter2);
+
+        assertEquals(0, novel.getNovelChapters().size());
+    }
+
+    @Test
+    public void testNovelHistories() {
+        Novel novel = new Novel();
+        NovelHistory novelHistory1 = new NovelHistory();
+        novelHistory1.setId(1L);
+        novel.addNovelHistory(novelHistory1);
+
+        assertEquals(1, novel.getNovelHistories().size());
+
+        NovelHistory novelHistory2 = new NovelHistory();
+        novelHistory2.setId(1L);
+        novel.removeNovelHistory(novelHistory2);
+
+        assertEquals(0, novel.getNovelHistories().size());
+    }
+
+    @Test
     public void testHashCode() {
         Novel novel1 = new Novel();
         novel1.setUrl("test1");
