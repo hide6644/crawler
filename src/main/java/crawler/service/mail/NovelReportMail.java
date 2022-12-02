308aa0cca0d4340c4296022cafdaebfca97f2b9c
hide6644 <hide6644@gmail.com>
2022-12-03 01:08:04 +0900

    ライブラリのバージョンアップ
    Spring Framework 6アップグレードに伴う修正を行った。

--- src/main/java/crawler/service/mail/NovelReportMail.java
+++ src/main/java/crawler/service/mail/NovelReportMail.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.mail.MessagingException;
+import jakarta.mail.MessagingException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
