c5804e99d84ec2dfeaea3d9b6cc0bacba76f5751
hide6644 <hide6644@gmail.com>
2024-04-04 14:20:40 +0900

    Lombok更新
    一部コードをLombokへ置き換えた。

--- src/test/java/crawler/service/impl/BaseManagerMockTestCase.java
+++ src/test/java/crawler/service/impl/BaseManagerMockTestCase.java
@@ -3,15 +3,13 @@ package crawler.service.impl;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import lombok.extern.log4j.Log4j2;
 
 @ExtendWith(MockitoExtension.class)
+@Log4j2
 public abstract class BaseManagerMockTestCase {
-
-    protected transient Logger log = LogManager.getLogger(this);
 
     protected ResourceBundle rb;
 
